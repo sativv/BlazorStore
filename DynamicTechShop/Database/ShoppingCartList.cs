b51fa9af8965b9cd249e0813a0bd68e3168563c2
David Vitas <david.vitas@hotmail.com>
2024-02-20 22:45:39 +0100

    init commit

---
+++ DynamicTechShop/Database/ShoppingCartList.cs
@@ -0,0 +1,10 @@
+﻿using DynamicTechShop.Models;
+using System.Net.Http.Headers;
+
+namespace DynamicTechShop.Database
+{
+    public static class ShoppingCartList
+    {
+        public static List<ProductModel> shoppingCart { get; set; } = new();
+    }
+}
---
+++ DynamicTechShop/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DynamicTechShop.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; } // Exempelvis: Smartphone, Laptop, Headphones
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+    }
+}
